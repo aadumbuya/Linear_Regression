9278d436c8dc7d00f3c8ec572d2c684063d004fa
unknown <a.dumbuya@alustudent.com>
2024-07-25 22:46:22 +0100

    cors

--- summative/API/main.py
+++ summative/API/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
 
@@ -18,6 +19,14 @@ class PredictionInput(BaseModel):
 
 # Create the FastAPI app
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 def predict(input_data: PredictionInput):
